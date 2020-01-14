eaa7ef1fb56bdd9aba7708fa040e7a76959b0965
Jiwon Han and Steven Fleming <steven@>
2020-01-14 13:50:16 -0800

    Complete html with buttons

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  $("#Javascript").click(function() {
+    $(".clickable").show();
+  });
+
+  $("#Operators").click(function() {
+    $(".clickable1").show();
+  });
+
+  $("#Variables").click(function() {
+    $(".clickable2").show();
+  });
+
+  $("#Variables2").click(function() {
+    $(".clickable3").show();
+  });
+
+  $("#Functions").click(function(){
+    $(".clickable4").show();
+  })
+
+  $("#Methods").click(function(){
+    $(".clickable5").show();
+  })
+
+  $("#Arguments").click(function(){
+    $(".clickable6").show();
+  })
+
+  $("#Parameters").click(function() {
+    $(".clickable7").show();
+  })
+
+  $("#Return").click(function(){
+    $(".clickable8").show();
+  })
+
+  $("#Chainingmethods").click(function(){
+    $(".clickable9").show();
+  })
+
+  $("#Strings").click(function(){
+    $(".clickable10").show();
+  })
+
+  $("#Booleans").click(function(){
+    $(".clickable11").show();
+  })
+
+  $("#Undefined").click(function(){
+    $(".clickable12").show();
+  })
+
+  $("#NaN").click(function(){
+    $(".clickable13").show();
+  })
+
+  $("#Escape").click(function(){
+    $(".clickable14").show();
+  })
+
+  $("#Alert").click(function(){
+    $(".clickable15").show();
+  })
+
+  $("#Comments").click(function(){
+    $(".clickable16").show();
+  })
+
+  $("#jQuery").click(function(){
+    $(".clickable17").show();
+  })
+
+  $("#Attributes").click(function(){
+    $(".clickable18").show();
+  })
+}); 
